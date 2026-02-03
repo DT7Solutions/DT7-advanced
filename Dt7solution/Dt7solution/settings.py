d84219ccb397caf213e1f562a9d3bfda37e61cb3
Chaitanya Kothakota <120007815+chaitanyakothakota@users.noreply.github.com>
2026-02-03 16:12:01 +0530

    Check the DT7 agency contact form submissions submitted with dt7.agency@gmail.com

--- Dt7solution/Dt7solution/settings.py
+++ Dt7solution/Dt7solution/settings.py
@@ -120,6 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = [
+    ('en-us', 'English (US)'),
+]
 
 TIME_ZONE = 'UTC'
 
